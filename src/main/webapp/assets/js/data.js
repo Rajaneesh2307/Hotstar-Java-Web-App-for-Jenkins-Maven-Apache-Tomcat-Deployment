3a8c77d4da683a9d62ad0141bcdd0da28cef22bf
Yembuluru Rajaneesh <101640141+Rajaneesh2307@users.noreply.github.com>
2026-04-15 15:35:51 +0530

    Update movie details in data.js

--- src/main/webapp/assets/js/data.js
+++ src/main/webapp/assets/js/data.js
@@ -1,27 +1,27 @@
 let movies = [
   {
-    name: "wanda vision",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "WandaVision",
+    des: "Wanda Maximoff creates a sitcom-like reality to live happily with Vision, but her illusion hides deeper truths and draws the attention of S.W.O.R.D.",
     image: "assets/img/slider 3.png",
   },
   {
-    name: "falcon and the winter soldier",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "The Falcon and the Winter Soldier",
+    des: "Sam Wilson and Bucky Barnes team up to face new global threats while grappling with the legacy of Captain America's shield.",
     image: "assets/img/slider 2.png",
   },
   {
-    name: "raya and the last dragon",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "Raya and the Last Dragon",
+    des: "In the land of Kumandra, warrior Raya embarks on a quest to find the last dragon and restore harmony against the evil Druun.",
     image: "assets/img/slider 4.png",
   },
   {
-    name: "loki",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "Loki",
+    des: "After escaping with the Tesseract, Loki is captured by the Time Variance Authority and forced to confront alternate timelines and his own destiny.",
     image: "assets/img/slider 1.png",
   },
   {
-    name: "luca",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    name: "Luca",
+    des: "On the Italian Riviera, young sea monster Luca experiences a magical summer of friendship, gelato, and scooter rides while hiding his true identity.",
     image: "assets/img/slider 5.png",
   },
 ];
